5c8153473104f9ed4cb0f19998e743b7f0dfd632
RETINA <gildasnfor@gmail.com>
2023-02-21 17:26:29 +0100

    deploy via jenkins

--- src/main/java/com/test/intern/InternApplication.java
+++ src/main/java/com/test/intern/InternApplication.java
@@ -2,13 +2,25 @@ package com.test.intern;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(value = "/")
+@CrossOrigin
 @SpringBootApplication(scanBasePackages={
 		"com.test.intern.canal","com.test.intern.serialization","com.test.intern.besoin"})
 public class InternApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InternApplication.class, args);
+	}
+
+	@GetMapping
+	String print() {
+		return "Backend for the ITNS task";
 	}
 
 }
