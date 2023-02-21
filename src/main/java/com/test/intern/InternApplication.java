fe130a1c995d127853ab82a87e1b66d8921a643d
RETINA <gildasnfor@gmail.com>
2023-02-21 18:53:38 +0100

    deploy via jenkins

--- src/main/java/com/test/intern/InternApplication.java
+++ src/main/java/com/test/intern/InternApplication.java
@@ -2,6 +2,7 @@ package com.test.intern;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @SpringBootApplication(scanBasePackages={
 		"com.test.intern.canal","com.test.intern.serialization","com.test.intern.besoin"})
-public class InternApplication {
+public class InternApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InternApplication.class, args);
